b5b4f71dfc6ce945c24ba9fae250b9623d868857
Peter Clarke <peter.clarke@yale.edu>
2024-09-04 18:43:18 -0400

    convert date utils to force using ISO 8601 format for years, and to use the same conversion math going from date -> seconds and from seconds -> date

--- src/main/ml-modules/root/utils/dateUtils.mjs
+++ src/main/ml-modules/root/utils/dateUtils.mjs
@@ -1,9 +1,5 @@
 import { BadRequestError } from '../lib/mlErrorsLib.mjs';
 
-const DAY_IN_SECONDS = 86400;
-const ONE_YEAR_IN_SECONDS = DAY_IN_SECONDS * 365; // purposely doesn't account for leap year
-const YEAR_ZERO_IN_SECONDS = new Date('0000-01-01T00:00:00Z').getTime() / 1000;
-
 const MIN_SUPPORTED_FULL_DATE_IN_SECONDS =
   new Date('-009999-01-01T00:00:00.000Z').getTime() / 1000;
 const MAX_SUPPORTED_FULL_DATE_IN_SECONDS =
@@ -50,17 +46,18 @@ function _completeDateTimeValue(dateTimeStr, start = true) {
     dateTimeStr = new String(dateTimeStr);
   }
   const origDateTimeStr = dateTimeStr;
-  dateTimeStr = _addLeadingZerosToDateTimeStr(dateTimeStr);
 
   // The optional hyphen (for B.C. / BCE) is in a group as it must be accounted for when determining how much
   // of the suffix to use.
-  const match = dateTimeStr ? dateTimeStr.match('^(-?)[0-9]{4,}') : null;
+  const match = dateTimeStr
+    ? dateTimeStr.match('^[0-9]{4,}|^(-?)[0-9]{6,}')
+    : null;
   if (match === null) {
     throw new BadRequestError(
       `Invalid date format: '${origDateTimeStr}'.
-      The format is 'YYYY-MM-DDThh:mm:ss' whereby at least the year must be specified.
-      Leading zeros in the year are optional.
-      Dates may be negative, denoting B.C.E. (a.k.a., B.C.).
+      The format is 'YYYY-MM-DDThh:mm:ss.000Z' or '-YYYYYY-MM-DDThh:mm:ss.000Z' for negative dates, whereby at least the year must be specified.
+      Leading zeros in the year are not optional.
+      Dates may be zero or negative, denoting B.C.E. (a.k.a., B.C.).
       When providing less than an entire xs.dateTime value, the system will populate the rest of the value based on the operator or whether the search tag is configured as a start (vs. end) date.
       When providing more than 'YYYY-MM-DD', quote the entire value.`
         .replace(/\n/g, ' ')
@@ -69,11 +66,12 @@ function _completeDateTimeValue(dateTimeStr, start = true) {
   }
   const year = match[0];
 
-  // When the given value's length is shorter than that of our targeted syntax (YYYY-MM-DDThh:mm:ssZ),
+  // When the given value's length is shorter than that of our targeted syntax (YYYY-MM-DDThh:mm:ss.000Z),
   // append the difference, taking into account whether the start or end is desired.
-  const targetedSyntaxLength = year.length + 16; // 16 is the length of either suffix
+  const targetedSyntaxLength = year.length + 20; // 20 is the length of either suffix
   if (dateTimeStr.length < targetedSyntaxLength) {
-    const suffix = start === true ? '-01-01T00:00:00Z' : '-12-31T23:59:59Z';
+    const suffix =
+      start === true ? '-01-01T00:00:00.000Z' : '-12-31T23:59:59.000Z';
     dateTimeStr =
       dateTimeStr +
       suffix.substring(
@@ -84,38 +82,18 @@ function _completeDateTimeValue(dateTimeStr, start = true) {
   return dateTimeStr;
 }
 
-function _addLeadingZerosToDateTimeStr(dateTimeStr) {
-  const isNegative = dateTimeStr.startsWith('-');
-  if (isNegative) {
-    dateTimeStr = dateTimeStr.slice(1);
-  }
-  const dateSplitHyphens = dateTimeStr.split('-');
-  //the first string is the year, pad it with 0s
-  if (/^\d+$/.test(dateSplitHyphens[0])) {
-    dateSplitHyphens[0] = dateSplitHyphens[0].padStart(4, '0');
-  }
-  dateTimeStr = dateSplitHyphens.join('-');
-  if (isNegative) {
-    dateTimeStr = `-${dateTimeStr}`;
-  }
-  return dateTimeStr;
-}
-
 function _convertDateTimeToSeconds(dateTimeStr) {
   const negativeDate = dateTimeStr.startsWith('-');
   const pieces = dateTimeStr.split('-');
   const year = parseInt(negativeDate ? `-${pieces[1]}` : pieces[0]);
 
   let totalSeconds;
-  if (year >= 1 && year < 10000) {
+  if (year >= -9999 && year <= 9999) {
     totalSeconds = new Date(dateTimeStr).getTime() / 1000;
   }
-  // Else, we exceeded JavaScript's Date support and need to calculate ourselves.
+  // Else, we exceeded our agreed full Date support and need to calculate ourselves.
   else {
-    totalSeconds = _adjustForPythonFromYear(
-      year,
-      _yearToApproximateSeconds(year, negativeDate, pieces)
-    );
+    totalSeconds = (year - 1970) * 365.2425 * 24 * 60 * 60;
   }
 
   if (isNaN(totalSeconds)) {
@@ -128,52 +106,6 @@ function _convertDateTimeToSeconds(dateTimeStr) {
   const roundedTotalSeconds = Math.round(totalSeconds);
 
   return roundedTotalSeconds;
-}
-
-function _adjustForPythonFromYear(year, secondsToAdjust) {
-  if (year <= -100 || year >= 10000) {
-    secondsToAdjust -= _getPythonAdjustmentInSeconds(year);
-  }
-  return secondsToAdjust;
-}
-
-// The JavaScript algorithm makes too large of negative numbers and too small of positive numbers,
-// when compared to Python's https://numpy.org/ library, which is used to calculate dates in
-// 64bit seconds within the data pipeline and thus documents.  The following adjustment gets
-// us closer to the Python values.  It is how much we should adjust for every 1,000 years and
-// was calculated by subtracting Python's value for -1000-01-01T00:00:00 from the above,
-// divided by 1, 000, and manually tweaked until we're only off 0.13 years per 65 million years.
-// Adjustment does not appear necessary until we hit year -0100.
-//
-// Here's the Python code we're trying to align with:
-//
-//   import numpy as np
-//   np.datetime64('-65000000-01-01T00:00:00').astype('<M8[s]').astype(np.int64 )
-//
-function _getPythonAdjustmentInSeconds(year) {
-  return 0.02055 * (year / 1000) * ONE_YEAR_IN_SECONDS;
-}
-
-function _yearToApproximateSeconds(year, negativeDate, pieces = null) {
-  // Before accounting for leap years, here is the number of seconds in the specified year.
-  const yearInSeconds = year * ONE_YEAR_IN_SECONDS;
-
-  // Calculate the rest of the date in seconds.
-  let restOfDateInSeconds = 0;
-  if (pieces) {
-    const restOfDateTimeStr = pieces
-      .slice(negativeDate ? 2 : 1, pieces.length)
-      .join('-');
-    const restOfDateStr = `1970-${restOfDateTimeStr}`;
-    restOfDateInSeconds = new Date(restOfDateStr).getTime() / 1000;
-    if (negativeDate) {
-      restOfDateInSeconds = restOfDateInSeconds * -1;
-    }
-  }
-
-  const leapYears = Math.ceil(year / 4) * DAY_IN_SECONDS;
-
-  return yearInSeconds + restOfDateInSeconds + leapYears + YEAR_ZERO_IN_SECONDS;
 }
 
 export { convertSecondsToDateStr, convertPartialDateTimeToSeconds };

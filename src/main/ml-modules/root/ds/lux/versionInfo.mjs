af8447300e9ff812c4c8ff39be064c58209d79f2
Peter Clarke <peter.clarke@yale.edu>
2024-09-13 11:58:29 -0400

    264: - data conversion date can come from random document instead of Yale University - if there is a tag for this commit, just display tag. Otherwise, include a number of commits and abbreviated tag name

--- src/main/ml-modules/root/ds/lux/versionInfo.mjs
+++ src/main/ml-modules/root/ds/lux/versionInfo.mjs
@@ -13,25 +13,9 @@ handleRequest(function () {
 function _getDataConversionDate() {
   try {
     return fn
-      .head(
-        cts
-          .doc(
-            fn.head(
-              sem.sparql(`
-      prefix la: <https://linked.art/ns/terms/>
-      
-      SELECT DISTINCT ?objectIri WHERE {
-        ?objectIri la:equivalent <http://vocab.getty.edu/ulan/500303558> ;
-      }
-      `)
-            ).objectIri
-          )
-          .xpath('/admin/conversion-date')
-      )
+      .head(cts.doc(fn.head(cts.uris())).xpath('/admin/conversion-date'))
       .toString();
   } catch (e) {
-    throw new Error(
-      "Can't get data version. Admin to check if there is a document matching expected criteria."
-    );
+    return 'error';
   }
 }
--- version.sh
+++ version.sh
@@ -1,7 +1,7 @@
 git -C . rev-parse 2> /dev/null
 
 if [ $? -eq 0 ]; then
-  git describe --tags --long
+  git describe --tags
 else
   echo "default"
 fi;

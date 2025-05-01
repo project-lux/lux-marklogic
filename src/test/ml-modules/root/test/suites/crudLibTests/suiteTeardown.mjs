b4627218d82d8b8f8b04b2e3db641a1c8ae35fc6
Brent Hartwig <brent_d_hartwig@hotmail.com>
2025-05-01 09:46:27 -0400

    #501: removed debugging check

--- src/test/ml-modules/root/test/suites/crudLibTests/suiteTeardown.mjs
+++ src/test/ml-modules/root/test/suites/crudLibTests/suiteTeardown.mjs
@@ -15,22 +15,6 @@ try {
     USERNAME_FOR_REGULAR_USER
   );
   removeExclusiveRolesByUsername(USERNAME_FOR_REGULAR_USER);
-
-  const zeroArityFun = () => {
-    const collectionName = 'userProfile';
-    console.log(
-      `Checking the '${collectionName}' collection within the ${xdmp.databaseName(
-        xdmp.database()
-      )} after delete...`
-    );
-    cts
-      .search(cts.collectionQuery(collectionName))
-      .toArray()
-      .forEach((doc) => {
-        console.log(`Found ${doc.baseURI}`);
-      });
-  };
-  xdmp.invokeFunction(zeroArityFun);
 } catch (e) {
   console.error(
     `crudLibTests/suiteTeardown.mjs encountered an error: ${e.message}`

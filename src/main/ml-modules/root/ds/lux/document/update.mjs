80f96bb44de706f08815985277e8fcf23380cf2c
Brent Hartwig <brent_d_hartwig@hotmail.com>
2025-06-23 14:43:22 -0400

    #546 and #547: update doc endpoint now requires the uri param and added "data/" in My Collections doc URIs

--- src/main/ml-modules/root/ds/lux/document/update.mjs
+++ src/main/ml-modules/root/ds/lux/document/update.mjs
@@ -2,10 +2,11 @@ import { handleRequest } from '../../../lib/securityLib.mjs';
 import { updateDocument } from '../../../lib/crudLib.mjs';
 
 const unitName = external.unitName;
+const uri = external.uri;
 const docNode = external.doc; // Do not use getObjectFromNode
 const lang = external.lang;
 
 handleRequest(function () {
   declareUpdate();
-  return updateDocument(docNode, lang);
+  return updateDocument(uri, docNode, lang);
 }, unitName);

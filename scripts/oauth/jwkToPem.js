c1280f671de250989fbb9a85f558ab8a8687ad0b
Peter Clarke <clarke.peterf@gmail.com>
2025-07-29 11:31:27 -0400

    update jwkToPem.js to generate full ML public-keys xml

--- scripts/oauth/jwkToPem.js
+++ scripts/oauth/jwkToPem.js
@@ -7,10 +7,18 @@
  * 3. Run `npm install`
  * 4. Insert the contents of ${COGNITO_URL}/.well-known/jwks.json file below
  * 5. Execute `node jwkToPem.js`
+ * 6. Console output can be copied into src/main/public-keys/oauth-${env}.xml
  */
 
 const jwkToPem = require('jwk-to-pem');
 const jwks = /*INSERT CONTENTS OF ${COGNITO_URL}/.well-known/jwks.json HERE*/
 
-const pems = jwks.keys.map((jwk) => jwkToPem(jwk));
+const pems = jwks.keys.map(
+  (jwk) => `  <oauth-jwt-secret>
+    <oauth-jwt-key-id>${jwk.kid}</oauth-jwt-key-id>
+    <oauth-jwt-secret-value>${jwkToPem(jwk).trim()}</oauth-jwt-secret-value>
+  </oauth-jwt-secret>`
+);
+console.log('<oauth-jwt-secrets>');
 pems.forEach((pem) => console.log(pem));
+console.log('</oauth-jwt-secrets>');

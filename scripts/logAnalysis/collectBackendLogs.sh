4567a031a9f85b0220e6deedc738d6ca1a38694f
Brent Hartwig <hartwig@progress.com>
2025-09-23 09:27:55 -0400

    Added support for downloading multiple days of logs.  Dropped support for downloading sar files. (#619)
    Co-authored-by: brent-hartwig <brent.d.hartwig@gmail.com>

--- scripts/logAnalysis/collectBackendLogs.sh
+++ scripts/logAnalysis/collectBackendLogs.sh
@@ -3,50 +3,132 @@
 #
 # Configuration
 #
-declare -a ipAddresses=("${INSERT}" "${IP_ADDRESSES}" "${HERE}")
+declare -a ipAddresses=("" "" "")
 # PEM file to access the source environment.
 pemFile=~/Apps/LUX/ML/ch-lux-ssh-prod.pem
 # Basenames of all files to pull down.  Omit the portion covered by inputBasenameSuffix.  Also incorporated into the output filenames.
 declare -a inputBasenames=("ErrorLog" "8003_ErrorLog" "8003_AccessLog" "8003_RequestLog" "8004_ErrorLog" "8004_AccessLog" "8004_RequestLog")
-# The suffix to add to the inputBasenames.  Use an empty string for today, _1 for yesterday, etc.
-inputBasenameSuffix=
+
+# Date or date range formatted as YYYYMMDD or YYYYMMDD-YYYYMMDD.  Used in the output filename.
+dateRange=20250804-20250818
+# Suffix associated to the *last* day in the date range
+inputBasenameSuffix=7
 # Output directory.  Omit trailing slash.
-outputDir=~/Apps/LUX/ML/test/20240521
-# Date of test formatted as YYYYMMDD.  Used in the output filename.
-testDate=20240521
-sarDate=2024-05-21
+outputDir=./logs
 # A reference to the source environment.  Used in the output filename.
-envName=blue-as-test
+envName=green-as-prod
 
 # Make the output directory when it doesn't already exist.
 [[ -d "$outputDir" ]] || mkdir "$outputDir"
 
 #
-# Execute commands on each node.
+# Parse date range and create arrays of dates and corresponding suffixes
+#
+if [[ "$dateRange" == *"-"* ]]; then
+  # Date range provided
+  IFS='-' read -ra DATE_PARTS <<< "$dateRange"
+  startDate=${DATE_PARTS[0]}
+  endDate=${DATE_PARTS[1]}
+else
+  # Single date provided
+  startDate=$dateRange
+  endDate=$dateRange
+fi
+
+# Function to increment date by one day
+increment_date() {
+  local date=$1
+  date -d "${date:0:4}-${date:4:2}-${date:6:2} + 1 day" +%Y%m%d 2>/dev/null || \
+  gdate -d "${date:0:4}-${date:4:2}-${date:6:2} + 1 day" +%Y%m%d
+}
+
+# Create arrays for dates and their corresponding suffixes
+declare -a dates=()
+declare -a suffixes=()
+
+currentDate=$startDate
+dateIndex=0
+
+while [[ $currentDate -le $endDate ]]; do
+  dates+=($currentDate)
+  
+  # Calculate suffix: most recent date gets inputBasenameSuffix, earlier dates get incremented values
+  if [[ $currentDate == $endDate ]]; then
+    suffixes+=($inputBasenameSuffix)
+  else
+    # Calculate how many days before the end date
+    daysBefore=$((dateIndex))
+    if [[ -n "$inputBasenameSuffix" ]]; then
+      calculatedSuffix=$((inputBasenameSuffix + (${#dates[@]} - 1 - dateIndex)))
+    else
+      calculatedSuffix=$((${#dates[@]} - 1 - dateIndex))
+    fi
+    suffixes+=($calculatedSuffix)
+  fi
+  
+  if [[ $currentDate == $endDate ]]; then
+    break
+  fi
+  
+  currentDate=$(increment_date $currentDate)
+  ((dateIndex++))
+done
+
+# Recalculate suffixes properly - most recent date uses inputBasenameSuffix, earlier dates increment
+declare -a correctedSuffixes=()
+totalDates=${#dates[@]}
+
+for (( i=0; i<${totalDates}; i++ )); do
+  if [[ $i == $((totalDates - 1)) ]]; then
+    # Last date (most recent) gets the original suffix
+    correctedSuffixes+=($inputBasenameSuffix)
+  else
+    # Earlier dates get incremented suffix values
+    if [[ -n "$inputBasenameSuffix" ]]; then
+      correctedSuffix=$((inputBasenameSuffix + (totalDates - 1 - i)))
+    else
+      correctedSuffix=$((totalDates - 1 - i))
+    fi
+    correctedSuffixes+=($correctedSuffix)
+  fi
+done
+
+#
+# Execute commands on each node for each date
 #
 ipAddressCnt=${#ipAddresses[@]}
 inputBasenamesCnt=${#inputBasenames[@]}
-for (( s=0; s<${ipAddressCnt}; s++ )); do
-  ipAddress=${ipAddresses[$s]}
+datesCnt=${#dates[@]}
 
-  # Iterate through the filenames
-  for (( t=0; t<${inputBasenamesCnt}; t++ )); do
-    # Underscores in the output filenames were problematic; switched to hyphens.
-    inputBasename=${inputBasenames[$t]}
-    outputBasename=$(echo "$inputBasename" | tr _ -)
+for (( d=0; d<${datesCnt}; d++ )); do
+  currentTestDate=${dates[$d]}
+  currentSuffix=${correctedSuffixes[$d]}
+  
+  echo "Processing date: $currentTestDate with suffix: $currentSuffix"
+  
+  for (( s=0; s<${ipAddressCnt}; s++ )); do
+    ipAddress=${ipAddresses[$s]}
 
-    # Last digits of IP address used in the output filename.  There is a potential chance of conflict.
-    ipAddressEnd=${ipAddress##*.}
-    
-    cmd="scp -i '$pemFile' 'ec2-user@$ipAddress:/var/opt/MarkLogic/Logs/$inputBasename$inputBasenameSuffix.txt' '$outputDir/$testDate-$envName-node-$ipAddressEnd-$outputBasename.txt'"
-    echo "Executing: $cmd"
-    eval $cmd
+    # Iterate through the filenames
+    for (( t=0; t<${inputBasenamesCnt}; t++ )); do
+      # Underscores in the output filenames were problematic; switched to hyphens.
+      inputBasename=${inputBasenames[$t]}
+      outputBasename=$(echo "$inputBasename" | tr _ -)
+
+      # Last digits of IP address used in the output filename.  There is a potential chance of conflict.
+      ipAddressEnd=${ipAddress##*.}
+      
+      # Add underscore prefix to suffix if it's not empty
+      suffix=""
+      if [[ -n "$currentSuffix" ]]; then
+        suffix="_$currentSuffix"
+      fi
+      
+      cmd="scp -i '$pemFile' 'ec2-user@$ipAddress:/var/opt/MarkLogic/Logs/$inputBasename$suffix.txt' '$outputDir/$currentTestDate-$envName-node-$ipAddressEnd-$inputBasename$suffix.txt'"
+      echo "Executing: $cmd"
+      eval $cmd
+    done
   done
-
-  # copy sar files
-   scmd="scp -i '$pemFile' ec2-user@$ipAddress:/tmp/sar*${sarDate}*gz '$outputDir/'"
-    echo "Executing: $scmd"
-    eval $scmd
 done
 
 echo ""

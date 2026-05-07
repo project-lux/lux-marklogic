668042f48bdf783ef0ed9c4e0850701fdd9d5055
brent-hartwig <brent.d.hartwig@gmail.com>
2026-05-07 14:16:53 -0400

    Wired facets endpoint directly to SCP.  Functional but does not yet return in ActivityStreams.  Pushing down into library next.

--- src/main/ml-modules/root/ds/lux/facets.mjs
+++ src/main/ml-modules/root/ds/lux/facets.mjs
@@ -1,5 +1,6 @@
 import { handleRequest } from '../../lib/securityLib.mjs';
-import { getFacet } from '../../lib/facetsLib.mjs';
+import { SearchCriteriaProcessor } from '../../lib/SearchCriteriaProcessor.mjs';
+import { FacetRequests } from '../../lib/search/FacetRequests.mjs';
 
 const unitName = external.unitName;
 const name = external.name;
@@ -10,14 +11,26 @@ const pageLength = external.pageLength;
 const sort = external.sort;
 
 const response = handleRequest(function () {
-  return getFacet({
-    facetName: name,
+  const searchCriteriaProcessor = new SearchCriteriaProcessor(false);
+  searchCriteriaProcessor.prepare(
     searchCriteria,
     searchScope,
-    page,
-    pageLength,
-    sort,
-  });
+    false, // allowMultiScope
+    null, // patternOptions
+    true, // includeTypeConstraint
+    1, // page for search results
+    1, // pageLength for search results
+    null, // pageWith
+    null, // sortCriteria
+    false, // valuesOnly
+  );
+
+  const facetRequests = new FacetRequests(page, pageLength);
+  facetRequests.addFacetRequest(name, sort);
+
+  const searchExecutionResult = searchCriteriaProcessor.execute(facetRequests);
+  const facetResponses = searchExecutionResult.getFacets();
+  return facetResponses ? facetResponses.getFacet(name) : null;
 }, unitName);
 
 response;

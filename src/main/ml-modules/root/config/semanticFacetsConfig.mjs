b0fe8ce01f6b33e11f408fa8986ee1ff6a402cdc
brent-hartwig <brent.d.hartwig@gmail.com>
2026-05-07 23:28:52 -0400

    First attempt at responsibleCollections

--- src/main/ml-modules/root/config/semanticFacetsConfig.mjs
+++ src/main/ml-modules/root/config/semanticFacetsConfig.mjs
@@ -2,23 +2,19 @@ import { IDENTIFIERS } from '../lib/identifierConstants.mjs';
 import op from '/MarkLogic/optic';
 import { getPrefixesForSPARQL } from '../lib/search/prefixUtils.mjs';
 
-const crm = op.prefixer('http://www.cidoc-crm.org/cidoc-crm/');
-const la = op.prefixer('https://linked.art/ns/terms/');
-const lux = op.prefixer('https://lux.collections.yale.edu/ns/');
-const skos = op.prefixer('http://www.w3.org/2004/02/skos/core#');
-
 const SEMANTIC_FACETS_CONFIG = {
   responsibleCollections: {
-    OBSOLETE_potentialFacetValuesCtsQuery: cts.andQuery([
-      cts.jsonPropertyValueQuery('dataType', 'Set', ['exact']),
-      cts.jsonPropertyValueQuery('id', IDENTIFIERS.collection, ['exact'], 1),
-    ]),
-    OBSOLETE_getValuesCountCtsQuery: (baseSearchCtsQuery, facetValueId) => {
-      return cts.andQuery([
-        cts.fieldValueQuery(['itemMemberOfId'], facetValueId, ['exact'], 1),
-        baseSearchCtsQuery,
-      ]);
-    },
+    plan: op.fromSPARQL(`
+      ${getPrefixesForSPARQL()}
+      SELECT ?item ?set
+      WHERE {
+        ?item la:member_of ?set .
+        ?set lux:setClassifiedAs <${IDENTIFIERS.collection}> .
+      }
+    `),
+    sourceJoinColumn: 'iri',
+    facetValueColName: 'set',
+    constraintJoinColumn: 'item',
     getFacetSelectedCriteria: (baseSearchJsonCriteria, facetValueId) => {
       const criteria = {
         _scope: 'item',
@@ -35,201 +31,6 @@ const SEMANTIC_FACETS_CONFIG = {
     },
   },
   responsibleUnits: {
-    OBSOLETE_potentialFacetValuesCtsQuery: cts.andQuery([
-      cts.jsonPropertyValueQuery('dataType', ['Group'], ['exact']),
-      cts.orQuery([
-        cts.andQuery([
-          cts.documentQuery(
-            cts
-              .triples(
-                [],
-                [lux('agentOfCuration')],
-                [],
-                '=',
-                ['eager', 'concurrent'],
-                cts.tripleRangeQuery(
-                  [],
-                  [lux('setClassifiedAs')],
-                  fn.insertBefore(
-                    cts.values(
-                      cts.iriReference(),
-                      '',
-                      ['eager', 'concurrent'],
-                      cts.fieldValueQuery(
-                        ['conceptIdentifier'],
-                        [IDENTIFIERS.collection],
-                        ['exact'],
-                        1,
-                      ),
-                    ),
-                    0,
-                    sem.iri('/does/not/exist'),
-                  ),
-                  '=',
-                  [],
-                  1,
-                ),
-              )
-              .toArray()
-              .map((x) => sem.tripleObject(x))
-              .concat(sem.iri('/does/not/exist')),
-          ),
-          cts.notQuery(
-            cts.andQuery([
-              cts.tripleRangeQuery(
-                [],
-                [lux('agentClassifiedAs')],
-                fn.insertBefore(
-                  cts.values(
-                    cts.iriReference(),
-                    '',
-                    ['eager', 'concurrent'],
-                    cts.fieldValueQuery(
-                      ['conceptIdentifier'],
-                      [IDENTIFIERS.department],
-                      ['exact'],
-                      1,
-                    ),
-                  ),
-                  0,
-                  sem.iri('/does/not/exist'),
-                ),
-                '=',
-                [],
-                1,
-              ),
-            ]),
-          ),
-        ]),
-        cts.andQuery([
-          cts.documentQuery(
-            cts
-              .triples(
-                [],
-                [crm('P107i_is_current_or_former_member_of')],
-                [],
-                '=',
-                ['eager', 'concurrent'],
-                cts.andQuery([
-                  cts.tripleRangeQuery(
-                    [],
-                    [lux('agentClassifiedAs')],
-                    fn.insertBefore(
-                      cts.values(
-                        cts.iriReference(),
-                        '',
-                        ['eager', 'concurrent'],
-                        cts.fieldValueQuery(
-                          ['conceptIdentifier'],
-                          [IDENTIFIERS.department],
-                          ['exact'],
-                          1,
-                        ),
-                      ),
-                      0,
-                      sem.iri('/does/not/exist'),
-                    ),
-                    '=',
-                    [],
-                    1,
-                  ),
-                  cts.documentQuery(
-                    cts
-                      .triples(
-                        [],
-                        [lux('agentOfCuration')],
-                        [],
-                        '=',
-                        ['eager', 'concurrent'],
-                        cts.tripleRangeQuery(
-                          [],
-                          [lux('setClassifiedAs')],
-                          fn.insertBefore(
-                            cts.values(
-                              cts.iriReference(),
-                              '',
-                              ['eager', 'concurrent'],
-                              cts.fieldValueQuery(
-                                ['conceptIdentifier'],
-                                [IDENTIFIERS.collection],
-                                ['exact'],
-                                1,
-                              ),
-                            ),
-                            0,
-                            sem.iri('/does/not/exist'),
-                          ),
-                          '=',
-                          [],
-                          1,
-                        ),
-                      )
-                      .toArray()
-                      .map((x) => sem.tripleObject(x))
-                      .concat(sem.iri('/does/not/exist')),
-                  ),
-                ]),
-              )
-              .toArray()
-              .map((x) => sem.tripleObject(x))
-              .concat(sem.iri('/does/not/exist')),
-          ),
-        ]),
-      ]),
-    ]),
-    OBSOLETE_getValuesCountCtsQuery: (baseSearchCtsQuery, facetValueId) => {
-      return cts.andQuery([
-        cts.andQuery([
-          cts.jsonPropertyValueQuery(
-            'dataType',
-            ['DigitalObject', 'HumanMadeObject', 'Set'],
-            ['exact'],
-          ),
-          cts.tripleRangeQuery(
-            [],
-            [la('member_of')],
-            fn.insertBefore(
-              cts.values(
-                cts.iriReference(),
-                '',
-                ['eager', 'concurrent'],
-                cts.tripleRangeQuery(
-                  [],
-                  [lux('agentOfCuration')],
-                  fn.insertBefore(
-                    cts.values(
-                      cts.iriReference(),
-                      '',
-                      ['eager', 'concurrent'],
-                      cts.orQuery([
-                        cts.fieldValueQuery(
-                          ['agentMemberOfId'],
-                          [facetValueId],
-                          ['exact'],
-                          1,
-                        ),
-                        cts.documentQuery([facetValueId]),
-                      ]),
-                    ),
-                    0,
-                    sem.iri('/does/not/exist'),
-                  ),
-                  '=',
-                  [],
-                  1,
-                ),
-              ),
-              0,
-              sem.iri('/does/not/exist'),
-            ),
-            '=',
-            [],
-            1,
-          ),
-        ]),
-        baseSearchCtsQuery,
-      ]);
-    },
     plan: op.fromSPARQL(`
       ${getPrefixesForSPARQL()}
       SELECT ?item ?set ?curator ?unit
